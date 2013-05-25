9bcad3f488379fd2bfb5fb9c76d88f94cc637cfe
Muhammet Halit Karakış <halidk@gmail.com>
2013-05-25 12:44:56 +0300

    ASFD

--- src/gyte/ooaad/mydailymemory/MainActivity.java
+++ src/gyte/ooaad/mydailymemory/MainActivity.java
@@ -1,16 +1,26 @@
 package gyte.ooaad.mydailymemory;
 
+import biz.halit.resimliyoklama.R;
+import biz.halit.resimliyoklama.activities.RollCallActivity;
+import biz.halit.resimliyoklama.datatypes.Student;
+import biz.halit.resimliyoklama.utils.Session;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -29,6 +39,8 @@ public class MainActivity extends Activity {
 			}
 		});
 		leftButton.setVisibility(View.VISIBLE);
+
+		setMemoryAdapter();
 	}
 
 	@Override
@@ -41,6 +53,35 @@ public class MainActivity extends Activity {
 	@Override
 	public void onBackPressed() {
 
+	}
+
+	private void setMemoryAdapter() {
+		// listView.setAdapter(new ArrayAdapter<Student>(this,
+		// R.layout.memlistitem, memories) {
+		// @Override
+		// public View getView(int position, View convertView, ViewGroup parent)
+		// {
+		// View row;
+		//
+		// if (convertView == null) {
+		// LayoutInflater inflater = (LayoutInflater) MainActivity.this
+		// .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		// convertView = inflater.inflate(R.layout.memlistitem,
+		// null);
+		// }
+		// row = convertView;
+		//
+		// TextView header = (TextView)
+		// row.findViewById(R.id.memlistitem_header);
+		// TextView text = (TextView) row
+		// .findViewById(R.id.memlistitem_text);
+		// header.setText("");
+		// text.setText("");
+		//
+		// return row;
+		//
+		// }
+		// });
 	}
 
 }
--- src/gyte/ooaad/mydailymemory/Session.java
+++ src/gyte/ooaad/mydailymemory/Session.java
@@ -2,4 +2,5 @@ package gyte.ooaad.mydailymemory;
 
 public class Session {
 	public static String username;
+	public static String memories;
 }
