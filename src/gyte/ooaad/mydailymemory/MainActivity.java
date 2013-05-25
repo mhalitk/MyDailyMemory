b3bb2c6b375d98297e5e742e897cd53e37c6d680
Muhammet Halit Karakış <halidk@gmail.com>
2013-05-25 10:13:10 +0300

    DEneme

--- src/gyte/ooaad/mydailymemory/MainActivity.java
+++ src/gyte/ooaad/mydailymemory/MainActivity.java
@@ -14,9 +14,10 @@ public class MainActivity extends Activity {
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
+		
+		//deneme
 	}
 
 }
