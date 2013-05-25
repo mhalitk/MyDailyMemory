73f188d9610f00ead8578d7d47e9afed38dcee34
Muhammet Halit Karakış <halidk@gmail.com>
2013-05-25 11:13:40 +0300

    Giriş sayfası oluşturuldu

--- src/gyte/ooaad/mydailymemory/Login.java
+++ src/gyte/ooaad/mydailymemory/Login.java
@@ -4,20 +4,18 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class Login extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.login);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
-		
-		//deneme
 	}
 
 }
